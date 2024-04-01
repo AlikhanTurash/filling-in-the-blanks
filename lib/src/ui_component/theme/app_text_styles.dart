dc37e122a777085cad79274d8a7cbd799f10e1e0
alikhanturash <AlikhanTurash@gmail.com>
2024-04-01 17:05:48 +0500

    test

--- lib/src/ui_component/theme/app_text_styles.dart
+++ lib/src/ui_component/theme/app_text_styles.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 abstract class AppTextStyles {
   static TextStyle buttonText = const TextStyle(
     fontSize: 30,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w700,
   );
 }
